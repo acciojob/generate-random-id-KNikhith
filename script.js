b667454ff69e8ad2d6c4b75eeb57c5436e9ba9d1
KNikhith <nikhithkotluri@gmail.com>
2026-03-23 11:51:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let range="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let ans=[];
+	for(let i=0;i<l;i++){
+		let random=Math.floor(Math.random()*range.length);
+		ans.push(range[random]);
+	}
+	return ans.join("");
 }
 
 // Do not change the code below.
